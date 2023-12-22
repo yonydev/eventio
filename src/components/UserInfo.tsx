65055f1ee0059f8b85f7a372c17b3e82b675e0eb
Yonatan Rojas <yonatan.rojas@crehana.com>
2023-12-21 23:51:38 -0600

    feat: created components folder

---
+++ src/components/UserInfo.tsx
@@ -0,0 +1,45 @@
+import { Routes } from "@blitzjs/next";
+import { useMutation } from "@blitzjs/rpc";
+import Link from "next/link";
+
+import logout from "@/auth/mutations/logout";
+
+import { useCurrentUser } from "@/users/hooks/useCurrentUser";
+
+const UserInfo = () => {
+  const currentUser = useCurrentUser();
+  const [logoutMutation] = useMutation(logout);
+
+  if (currentUser) {
+    return (
+      <>
+        <button
+          className=""
+          onClick={async () => {
+            await logoutMutation();
+          }}
+        >
+          Logout
+        </button>
+        <div>
+          User id: <code>{currentUser.id}</code>
+          <br />
+          User role: <code>{currentUser.role}</code>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Link href={Routes.SignupPage()} className="">
+          <strong>Sign Up</strong>
+        </Link>
+        <Link href={Routes.LoginPage()} className="">
+          <strong>Login</strong>
+        </Link>
+      </>
+    );
+  }
+};
+
+export default UserInfo;
